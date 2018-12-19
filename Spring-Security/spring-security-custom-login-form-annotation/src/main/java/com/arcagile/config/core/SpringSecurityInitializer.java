6c69040ff3e184f723b90e6d95adb682491bcb27
Syed Maqbul Corptrainer <syedmaqbul.corptrainer2016@gmail.com>
2018-12-20 00:38:41 +0530

    Spring Security examples

---
+++ Spring-Security/spring-security-custom-login-form-annotation/src/main/java/com/arcagile/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.arcagile.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
